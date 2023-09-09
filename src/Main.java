d076139368ad7035223613698c47e9948ff1a2c9
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-09-09 03:05:36 +0300

    Part 1

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
      boolean AirAlarm = false ;
+
      boolean DayOfStudying = true ;
+
      boolean IamInKyiv = true ;
 
      boolean GoToAUK = true ;
+
      GoToAUK = ! AirAlarm  && DayOfStudying && IamInKyiv;
+
      System.out.println(GoToAUK);
 
 
